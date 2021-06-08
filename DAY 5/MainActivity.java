2b04d1d40a0493c4838eabba931ad7115a4164c6
Suryadipsinhjadeja <66167674+Suryadipsinhjadeja@users.noreply.github.com>
2021-06-08 20:48:10 +0530

    Create MainActivity.java

---
+++ DAY 5/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.myapplication4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button btnclick;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnclick=findViewById(R.id.btnclick);
+
+        btnclick.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,data.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
