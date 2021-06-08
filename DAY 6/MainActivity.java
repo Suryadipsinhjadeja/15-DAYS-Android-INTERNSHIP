ab6fedaf5a0a132c85e4f8c97e72c3e639785401
Suryadipsinhjadeja <66167674+Suryadipsinhjadeja@users.noreply.github.com>
2021-06-08 17:18:09 +0530

    Create MainActivity.java

---
+++ DAY 6/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.myapplication2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+
+
+        Thread thread=new Thread(){
+            public void run(){
+                try {
+                    sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }finally {
+                    Intent intent=new Intent(MainActivity.this,Splash.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        thread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
+    }
+}
