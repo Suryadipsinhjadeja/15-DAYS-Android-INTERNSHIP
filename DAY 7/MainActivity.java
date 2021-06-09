44fae1da3f7165b0d149545fe927fff1cdbf0bda
Suryadipsinhjadeja <66167674+Suryadipsinhjadeja@users.noreply.github.com>
2021-06-09 10:59:11 +0530

    For list view

---
+++ DAY 7/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.list_view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+ListView lv1;
+
+String name[]={"Bharatiya Janata Party","Indian National Congress","Aam Aadmi Party","Shiv Sena","All India Trinamool Congress","Janata Dal","Rashtriya Janata Dal","National Democratic Alliance","United Progressive Alliance"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        lv1=findViewById(R.id.lv1);
+
+
+        ArrayAdapter arrayAdapter=new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1,name);
+
+
+        lv1.setAdapter(arrayAdapter);
+    }
+}
